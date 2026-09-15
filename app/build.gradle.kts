0d6cf453c323346caddce66eadeebfb6e2088661
pri4esh-ai <pri4esh@gmail.com>
2026-09-15 19:36:46 +0530

    Update build.gradle.kts for NDK and Kotlin options

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,14 +10,36 @@ android {
 
     defaultConfig {
         applicationId = "com.sana.android"
+
         minSdk = 26
         targetSdk = 35
 
         versionCode = 1
         versionName = "1.0"
 
-        vectorDrawables {
-            useSupportLibrary = true
+        ndk {
+            abiFilters += listOf("arm64-v8a")
+        }
+
+        externalNativeBuild {
+            cmake {
+                cppFlags += listOf(
+                    "-std=c++17"
+                )
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+
+            proguardFiles(
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
+                "proguard-rules.pro"
+            )
         }
     }
 
@@ -25,22 +47,49 @@ android {
         compose = true
     }
 
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.31.6"
+        }
+    }
+
     packaging {
+        jniLibs {
+            useLegacyPackaging = false
+        }
+
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
+
     implementation("androidx.activity:activity-compose:1.10.1")
 
-    implementation(platform("androidx.compose:compose-bom:2025.01.00"))
+    implementation(
+        platform(
+            "androidx.compose:compose-bom:2025.01.00"
+        )
+    )
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
-    debugImplementation("androidx.compose.ui:ui-tooling")
-}w
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling"
+    )
+}
